6a3e3a59dc55495f6a9ead72dd28aa6eddd3fb43
Janaury <january147@outlook.com>
2017-09-24 13:54:49 +0800

    增加了置换密码算法

--- c_encode.c
+++ c_encode.c
@@ -1,7 +1,8 @@
-/*charset:utf8*/
+﻿/*charset:utf8*/
 #include<stdio.h>
 #include<stdlib.h>
 
+/*base64编码-begin:*/
 /*base64编码表*/
 const char *base64_char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
@@ -207,4 +208,77 @@ void base64_file_decode(const char* encode_filepath,const char* origin_filepath)
 	fclose(fp1);
 	fclose(fp2);
 }
+/*base64编码-end:*/
 
+/*置换密码编码-begin:*/
+int permutation_encode(const char* plain_text,int len,const char* key,int key_len,char** result)
+{
+	int p_cipher = 0;
+	int p_key = 0;
+	int p_plain = key[p_key]-'1';
+	int space;
+	int stuff_len = len%key_len;
+	char* cipher_text;
+	if(stuff_len==0)
+		space = len;
+	else
+		space = (len/key_len+1)*key_len;
+	cipher_text = (char*)malloc(space+1);
+	for(;p_cipher<space;p_cipher++)
+	{
+		if(p_plain<len)/*正常读取*/ 
+			cipher_text[p_cipher] = plain_text[p_plain];
+		else if(p_plain>=space)/*该列已经读取完毕*/ 
+		{
+			p_key++;
+			p_plain = key[p_key]-'1';
+			cipher_text[p_cipher] = plain_text[p_plain];
+		}
+		else/*该列最后一行没有字符*/ 
+		{
+			p_key++;
+			p_plain = key[p_key]-'1';
+			cipher_text[p_cipher] = '@';
+			continue;	
+		}
+		/*取该列下一个字符*/
+		p_plain = p_plain + key_len;
+	}
+	cipher_text[space] = '\0';
+	*result = cipher_text;
+	return space;
+}
+
+int permutation_decode(char* cipher_text,int len,char* key,int key_len,char** result)
+{
+	int p_cipher = 0;
+	int p_key = 0;
+	int p_plain = key[p_key]-'1';
+	int plain_text_len = len;
+	char* plain_text;
+
+	plain_text = (char*)malloc(len);
+	for(;p_cipher<len;p_cipher++)
+	{
+		
+		if(p_plain>=len)/*该列已经读取完毕*/ 
+		{
+			p_key++;
+			p_plain = key[p_key]-'1';
+			plain_text[p_plain] = cipher_text[p_cipher];
+		}
+		else/*正常读取*/ 
+		{
+			if(cipher_text[p_cipher] == '@')
+				plain_text_len--;
+			plain_text[p_plain] = cipher_text[p_cipher];
+		}
+			
+		p_plain = p_plain + key_len;
+	}
+	plain_text[plain_text_len] = '\0';
+	*result = plain_text;
+	return plain_text_len;
+ } 
+
+/*置换密码-end:*/
--- c_encode.h
+++ c_encode.h
@@ -26,3 +26,22 @@ void base64_file_encode(const char* origin_filepath,const char* encode_filepath)
 void base64_file_decode(const char* encode_filepath,const char* origin_filepath);
 
 /*base64编码*/ 
+
+/*置换密码编码
+*permutation_encode(plain_text,len,key,key_len,&result)
+*@para1:字符串明文
+*@para2:该明文的长度
+*@para3:以字符串数字表示的密钥，即读取列的顺序
+*@para4:密钥的长度，也是置换中矩阵行的长度
+*@para5:用来接收结果的二级指针（传入参数时使用&+一级指针）
+*返回加密后字符串的长度
+*
+*permutation_decode(cipher_text,len,key,key_len,&result)
+*参数与加密函数类似，区别是第一个为密文字符串
+*返回解密后明文的长度
+*/
+
+int permutation_encode(const char* plain_text,int len,const char* key,int key_len,char** result);
+int permutation_decode(char* cipher_text,int len,char* key,int key_len,char** result);
+
+/*置换密码编码*/
